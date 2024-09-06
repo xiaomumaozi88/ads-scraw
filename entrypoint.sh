8ac8c8c6200a4ce9978ab50cc63e3f8b9c68968a
qiujian <2317510668@qq.com>
2024-09-06 11:00:32 +0800

    feat: 修改启动指令，去除图形化界面
    ARK-6780 ARK-6832 ARK-6776

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export DISPLAY=:1
-
-rm -f /tmp/.X1-lock
-Xvfb :1 -ac -screen 0 1024x768x16 &
-
-x11vnc -ncache_cr -display :1 -forever -shared  -bg -noipv6 -passwd "$VNC_SERVER_PASSWORD" &
-
-npm start
