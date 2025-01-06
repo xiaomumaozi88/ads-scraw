a38b14a2318c15cef8ff8b574fb317898caa0d97
qiujian <2317510668@qq.com>
2025-01-06 18:15:59 +0800

    feat: 优化常量定义,v2
    ARK-9999

--- src/controllers/scraperController.js
+++ src/controllers/scraperController.js
@@ -1,6 +1,7 @@
 import * as puppeteerService from '../services/puppeteerService.js';
 import * as puppeteerServiceA3 from '../services/puppeteerServiceA3.js';
-import {A3_GOOGLE_ACCOUNT} from "../constants";
+
+const A3_GOOGLE_ACCOUNT = 'infocenter@a3games.com';
 
 export const scrape = async (req, res) => {
     const { orderId, accountId, account} = req.body;
